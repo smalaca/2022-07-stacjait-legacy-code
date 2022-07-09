b657edf8a69cb52a52847c638fac29ded5ec0fc6
smalaca <ab13.krakow@gmail.com>
2022-07-09 14:29:27 +0200

    introduce AntiCorruptionLayer and convert UserCommandRepository into interface

---
+++ src/main/java/com/smalaca/acl/AntiCorruptionLayer.java
@@ -0,0 +1,38 @@
+package com.smalaca.acl;
+
+import com.smalaca.cqrs.taskmanager.command.user.UserCommandRepository;
+import com.smalaca.taskamanager.exception.UserNotFoundException;
+import com.smalaca.taskamanager.model.entities.User;
+import com.smalaca.taskamanager.repository.UserRepository;
+
+import java.util.Optional;
+
+public class AntiCorruptionLayer implements UserCommandRepository {
+    private final UserRepository userRepository;
+
+    public AntiCorruptionLayer(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public boolean notExistByFirstAndLastName(String firstName, String lastName) {
+        return userRepository.findByUserNameFirstNameAndUserNameLastName(firstName, lastName).isEmpty();
+    }
+
+    @Override
+    public Long save(User user) {
+        User saved = userRepository.save(user);
+        return saved.getId();
+    }
+
+    @Override
+    public User findById(Long id) {
+        Optional<User> found = userRepository.findById(id);
+
+        if (found.isEmpty()) {
+            throw new UserNotFoundException();
+        }
+
+        return found.get();
+    }
+}
--- src/main/java/com/smalaca/cqrs/taskmanager/command/user/UserCommandRepository.java
+++ src/main/java/com/smalaca/cqrs/taskmanager/command/user/UserCommandRepository.java
@@ -1,34 +1,11 @@
 package com.smalaca.cqrs.taskmanager.command.user;
 
-import com.smalaca.taskamanager.exception.UserNotFoundException;
 import com.smalaca.taskamanager.model.entities.User;
-import com.smalaca.taskamanager.repository.UserRepository;
 
-import java.util.Optional;
+public interface UserCommandRepository {
+    boolean notExistByFirstAndLastName(String firstName, String lastName);
 
-public class UserCommandRepository {
-    private final UserRepository userRepository;
+    Long save(User user);
 
-    public UserCommandRepository(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    boolean notExistByFirstAndLastName(String firstName, String lastName) {
-        return userRepository.findByUserNameFirstNameAndUserNameLastName(firstName, lastName).isEmpty();
-    }
-
-    Long save(User user) {
-        User saved = userRepository.save(user);
-        return saved.getId();
-    }
-
-    User findById(Long id) {
-        Optional<User> found = userRepository.findById(id);
-
-        if (found.isEmpty()) {
-            throw new UserNotFoundException();
-        }
-
-        return found.get();
-    }
+    User findById(Long id);
 }
